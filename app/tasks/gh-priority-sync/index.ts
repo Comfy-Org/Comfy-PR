3957f98750b3d647ee47aa7553e30e032a80d7bf
snomiao <snomiao@gmail.com>
2025-12-22 04:45:00 +0000

    fix: libs refactor

--- app/tasks/gh-priority-sync/index.ts
+++ app/tasks/gh-priority-sync/index.ts
@@ -33,7 +33,7 @@
  * - Gracefully handles errors for individual label or property update operations without stopping
  *   the entire task.
  */
-import { github, notion } from "@/app/libs";
+import { github, notion } from "@/lib";
 import { db } from "@/src/db";
 import type { GH } from "@/src/gh";
 import { ghPageFlow } from "@/src/ghPageFlow";
